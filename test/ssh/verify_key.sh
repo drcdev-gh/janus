242e1fa9ee622828b5e5b53610ee452c1bb15e6e
drcdev-gh <631758+drcdev-gh@users.noreply.github.com>
2026-03-06 15:53:29 +0900

    Rename to janus

--- test/ssh/verify_key.sh
+++ test/ssh/verify_key.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -s -G --data-urlencode "pubkey=$1 $2" -H "x-api-key:helloworld" http://janus-sync-janus-sync-1:8085/ssh/validate
+curl -s -G --data-urlencode "pubkey=$1 $2" -H "x-api-key:helloworld" http://janus-janus-1:8085/ssh/validate
